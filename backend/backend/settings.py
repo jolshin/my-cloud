104cc6d4129b77d98a4d29ea95a37c46fc0084c5
System Administrator <root@Mac-mini.IGD_Rostelecom>
2025-08-26 19:12:32 +0800

    nginx log formats and set added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,20 +96,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-
     # Postgresql settings
-    'default': { 
+    'localhost': { 
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432', 
-    }
+    },
+
+    # # Postgresql settings
+    # 'production': { 
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'DATABASE_NAME',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432', 
+    # }
 }
 
 
